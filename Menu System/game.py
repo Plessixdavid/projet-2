a7f32e78959ac47aa5e0f305a140df833c800da7
petite-souris <cecilia.conquet@gmail.com>
2021-11-09 15:00:51 +0100

    Variables to display the background.

--- Menu System/game.py
+++ Menu System/game.py
@@ -15,3 +15,13 @@ class Game():
         self.playing = False          # When the player really gamed.
         # All keys are false until the player choose one and become true.
         self.UP_KEY, self.DOWN_KEY, self.START_KEY, self.BACK_KEY = False, False, False, False
+
+        # Variables to display the background :
+        self.display_width_min = 800
+        self.display_width_max = 1980
+        self.display_height_min = 800
+        self.display_height_max = 1080
+        self.display = pygame.surface(self.display_width_min, self.display_height_min)
+        self.window = pygame.display.set_mode(self.display_width_min, self.display_height_min)
+
+       
