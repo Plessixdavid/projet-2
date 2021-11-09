71ef2c3cb172c05246f1b83bcf19ecf21b798438
petite-souris <cecilia.conquet@gmail.com>
2021-11-09 14:52:09 +0100

    Creation of variables for the menu.

--- Menu System/game.py
+++ Menu System/game.py
@@ -6,5 +6,12 @@ import pygame
 # Creation of the class Game.
 class Game():
     def __init__(self):
+
         # To have all the fonctionnalities of pygame.
         pygame.init()
+
+        # Variables of menu :
+        self.running = True           # When the game is started but not played.
+        self.playing = False          # When the player really gamed.
+        # All keys are false until the player choose one and become true.
+        self.UP_KEY, self.DOWN_KEY, self.START_KEY, self.BACK_KEY = False, False, False, False
