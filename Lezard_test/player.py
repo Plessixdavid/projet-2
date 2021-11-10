b81166187322e74e970cdb938aafd415da4482e8
Lezard91 <jordanlenfantbenard@gmail.com>
2021-11-10 14:12:00 +0100

    branch_test

---
+++ Lezard_test/player.py
@@ -0,0 +1,20 @@
+import pygame
+
+class Player(pygame.sprite.Sprite):
+
+    def __init__(self):
+        super().__init__()
+        self.health = 100
+        self.max_health = 100
+        self.attack = 10
+        self.velocity = 1
+        self.image = pygame.image.load("Lezard_test/test_icon_player.png")
+        self.rect = self.image.get_rect()
+        self.rect.x = 200
+        self.rect.y = 200
+
+    def move_right(self):
+        self.rect.x += self.velocity
+
+    def move_left(self):
+        self.rect.x -= self.velocity
