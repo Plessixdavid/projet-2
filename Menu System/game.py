bda05c77e3a0a7152278e3610dab7105e6aed854
petite-souris <cecilia.conquet@gmail.com>
2021-11-16 14:07:52 +0100

    Modifications about the variables for display.

--- Menu System/game.py
+++ Menu System/game.py
@@ -12,17 +12,15 @@ class Game():
 
         # Variables of menu :
         self.running = True           # When the game is started but not played.
-        self.playing = False          # When the player really gamed.
+        self.playing = False          # When the player really start the game.
         # All keys are false until the player choose one and become true.
         self.UP_KEY, self.DOWN_KEY, self.START_KEY, self.BACK_KEY = False, False, False, False
 
         # Variables to display the background :
-        self.display_width_min = 800
-        self.display_width_max = 1980
-        self.display_height_min = 800
-        self.display_height_max = 1080
-        self.display = pygame.surface(self.display_width_min, self.display_height_min)
-        self.window = pygame.display.set_mode(self.display_width_min, self.display_height_min)
+        self.display_width = 800
+        self.display_height = 800
+        self.display = pygame.surface(self.display_width, self.display_height)
+        self.window = pygame.display.set_mode(self.display_width, self.display_height)
 
         # Font for the menu :
         self.font_name = '8-BIT WONDER.TTF'
