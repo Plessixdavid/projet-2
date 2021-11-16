b018788ae7bd99afbe5cb5a883aee578aeb99437
petite-souris <cecilia.conquet@gmail.com>
2021-11-16 16:20:21 +0100

    Insignificants changes.

--- Menu System/main.py
+++ Menu System/main.py
@@ -1,11 +1,12 @@
 # Coding : utf-8
 
 # Imports :
-from game import Game
+from game import game
 
 # Attribution of the function Game :
-g = Game()
+g = game()
 
 # Loop when the game is running :
 while g.running:
+    g.playing = True
     g.game_loop()
