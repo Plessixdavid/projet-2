9e2cf7efdef666bcb9769754a0522eaa519571dc
petite-souris <cecilia.conquet@gmail.com>
2021-11-19 14:48:15 +0100

    Fix bugs.

--- Menu System/main.py
+++ Menu System/main.py
@@ -1,6 +1,7 @@
 # Coding : utf-8
 
 # Imports :
+import pygame
 from game import game
 
 # Attribution of the function Game :
@@ -8,5 +9,6 @@ g = game()
 
 # Loop when the game is running :
 while g.running:
+    pygame.init()
     g.playing = True
     g.game_loop()
