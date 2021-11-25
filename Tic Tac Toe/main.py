216436bc0f38a1f5a909e61b11df82a90386ec77
petite-souris <cecilia.conquet@gmail.com>
2021-11-25 21:07:26 +0100

    Event right click of the mouse + position of the mouse.

--- Tic Tac Toe/main.py
+++ Tic Tac Toe/main.py
@@ -26,7 +26,17 @@ class Game :
                 # Quit the game :
                 if event.type == pygame.QUIT:
                     sys.exit()
-
+                # Event : right click : 
+                    # [0] : list of buttons in the mouse = right click. 
+                if event.type == pygame.MOUSEBUTTONDOWN and pygame.mouse.get_pressed()[0]:   
+                    # Get the position of the mouse :
+                    position = pygame.mouse.get_pos()
+                    # Position x of the mouse :
+                    position_x = position[0]//200
+                    # Position y of the mouse :
+                    position_y = position[1]//200
+                    
+                    print(position_x, position_y)
             # Colors of the windows :
             self.screen.fill((240, 240, 240))
 
