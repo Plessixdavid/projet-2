96e812c7d25312ec89973435d72b0671e0693deb
petite-souris <cecilia.conquet@gmail.com>
2021-11-25 21:44:55 +0100

    Draw the X and the O.

--- Tic Tac Toe/grid.py
+++ Tic Tac Toe/grid.py
@@ -20,6 +20,30 @@ class Grid:
         # Init the grid :
         self.grid = [[None in x in range(0, 3) for y in range(0, 3)]]
 
+    # New function to display the lines :
+    def display_lines(self):
+        # For each lines in self :
+        for line in self.lines :
+            # Display the lines :
+                # Function pygame + color + start + end + thickness.
+            pygame.draw.line(self.screen, (0, 0, 0), line[0], line[1], 2)
+
+        # Display X and O :
+            # Loop through the lines :
+        for y in range(0, len(self.grid)):
+            # Loop through the columns :
+            for x in range(0, len(self.grid)):
+                # Condition to draw the lines :
+                if self.grid[y][x] == 'X':
+                    # Pygame + draw line + black color + start position + end position + thickness.
+                    pygame.draw.line(self.screen, (0, 0, 0), (x * 200, y * 200), (200 * (x * 200), 200 * (y * 200)), 3)
+                    pygame.draw.line(self.screen, (0, 0, 0), ((x * 200), 200 * (y * 200)), (200 * (x * 200), (y * 200)), 3)
+                elif self.grid[y][x] == 'O':
+                    # Pygame + draw circle + black color + start position + end position + radius + thickness.
+                    pygame.draw.circle(self.screen, (0, 0, 0), (100 * (x * 200), 100 * (y * 200)), 20, 3)
+                    pygame.draw.circle(self.screen, (0, 0, 0), ((x * 200), 200 * (y * 200)), (200 * (x * 200), (y * 200)), 3)
+
+
     # New function to print the grid :
     def print_grid(self):
         print(self.grid)
@@ -27,11 +51,3 @@ class Grid:
     # New function to stare the values :
     def stare_the_value(self, x, y, value):
         self.grid[y][x] = value
-
-    # New function to display the lines :
-    def display_lines(self):
-        # For each lines in self :
-        for line in self.lines :
-            # Display the lines :
-            # Function pygame + color + start + end + thickness.
-            pygame.draw.line(self.screen, (0, 0, 0), line[0], line[1], 2)
