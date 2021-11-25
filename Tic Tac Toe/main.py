b6ac17000880158db849419b464280ce5580f368
petite-souris <cecilia.conquet@gmail.com>
2021-11-25 20:31:28 +0100

    Loop when the game is running.

--- Tic Tac Toe/main.py
+++ Tic Tac Toe/main.py
@@ -8,3 +8,6 @@ class Jeu :
     def __init__(self):
         self.ecran = pygame.display.set_mode((600, 600))
         pygame.display.set_caption('Tic Tac Toe')
+        self.game_running = True
+
+    
