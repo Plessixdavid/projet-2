28187488f473b53f20cacc320d8ab371844918ef
petite-souris <cecilia.conquet@gmail.com>
2021-11-25 20:38:18 +0100

    Main function in the main.

--- Tic Tac Toe/main.py
+++ Tic Tac Toe/main.py
@@ -4,10 +4,30 @@
 import pygame
 import sys
 
-class Jeu :
+# New class:
+class Game :
+    # Function init :
     def __init__(self):
+        # Pygame's windows :
         self.ecran = pygame.display.set_mode((600, 600))
+        # Name of the game :
         pygame.display.set_caption('Tic Tac Toe')
+        # Loop when the game is running :
         self.game_running = True
 
-    
+    # Main function :
+    def Main_function(self) :
+        # Loop :
+        while self.game_running:
+            # To receive all elements in pygame :
+            for event in pygame.event.get():
+                # Quit the game :
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+        # Colors of the windows :
+        self.ecran.fill((240, 240, 240))
+
+        # Update the screen :
+        pygame.display.flip()
+
