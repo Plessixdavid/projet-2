745eb0064c0830ed6a15c6c414f474082b33490c
petite-souris <cecilia.conquet@gmail.com>
2021-11-25 20:47:37 +0100

    Function init for the class Grid.

--- Tic Tac Toe/grid.py
+++ Tic Tac Toe/grid.py
@@ -2,3 +2,16 @@
 
 # Imports :
 import pygame
+
+# New class :
+class Grid:
+    # Function init :
+    def __init__(self):
+        # Lines of the grids :
+        self.lines = [                  # List.
+            ((200, 0), (200, 600)),     # First line.
+            ((400, 0),(400, 600)),      # Second line.
+            ((0, 200), (600, 200)),     # Third line.
+            ((0, 400), (600, 400))      # Fourth line.
+            ]
+
