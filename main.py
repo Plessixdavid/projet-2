c0f93a25a77baa653f61b0862d2dd5bbc994e5c6
Fassi Théo <gtheya@gmail.com>
2021-12-01 15:08:58 +0100

    changed url

--- main.py
+++ main.py
@@ -14,13 +14,8 @@ if __name__ == "__main__":
         print('connection established')
 
     @sio.event
-    def my_message(data):
-        print('message received with ', data)
-        sio.emit('my response', {'response': 'my response'})
-
-    @sio.event
     def disconnect():
         print('disconnected from server')
 
-    sio.connect('http://localhost:6969')
+    sio.connect('http://93.6.41.243:8271/')
     game.run(sio)
