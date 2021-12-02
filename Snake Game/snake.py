ea186c1460a7064af9071356e4f8732155f163c6
petite-souris <cecilia.conquet@gmail.com>
2021-12-02 10:34:00 +0100

    Position + direction of the snake.

--- Snake Game/snake.py
+++ Snake Game/snake.py
@@ -4,5 +4,9 @@
 
 # New function : Snake :
 def Snake_position():
-    Snake_position_x = 300
-    Snake_position_y = 300
+    # Initial position of the snake in the screen :
+    snake_position_x = 300
+    snake_position_y = 300
+    # Position for the direction of the snake : 
+    snake_direction_x = 0
+    snake_direction_y = 0
