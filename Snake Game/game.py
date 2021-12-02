9a516a11e3b730f573a08913e25f2d1e3d237333
petite-souris <cecilia.conquet@gmail.com>
2021-12-02 10:55:13 +0100

    Movements of the snake on the screen.

--- Snake Game/game.py
+++ Snake Game/game.py
@@ -34,7 +34,7 @@ class Game :
                 # Close the windows :
                 if evenement.type == pygame.QUIT:
                     sys.exit()
-                    
+
                 # Event KEYDOWN :
                 if evenement.type == pygame.KEYDOWN:
                     # Event K_RIGHT :
@@ -43,16 +43,23 @@ class Game :
                         self.snake_direction_y = 0
                     # Event K_LEFT :
                     if evenement.key == pygame.K_LEFT:
-                        self.serpent_direction_x = -10
-                        self.serpent_direction_y = 0
+                        self.snake_direction_x = -10
+                        self.snake_direction_y = 0
                     # Event K_DOWN :
                     if evenement.key == pygame.K_DOWN:
-                        self.serpent_direction_y = 10
-                        self.serpent_direction_x = 0
+                        self.snake_direction_y = 10
+                        self.snake_direction_x = 0
                     # Event K_UP :
                     if evenement.key == pygame.K_UP:
-                        self.serpent_direction_y = -10
-                        self.serpent_direction_x = 0
-        
-            # Update the screen :     
-            pygame.display.flip()
+                        self.snake_direction_y = -10
+                        self.snake_direction_x = 0
+
+    # New function about movements of the snake :       
+    def snake_movement(self):
+        # Make the snake moves at right or at left on the screen :
+        self.snake_position_x += self.snake_direction_x
+        # Make the snake moves at up or at down on the screen :
+        self.snake_position_y += self.snake_direction_y  
+            
+    # Update the screen :     
+    pygame.display.flip()
