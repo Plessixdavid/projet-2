308d2fd1fccc094d12563990623c5c806d22d14f
petite-souris <cecilia.conquet@gmail.com>
2021-12-02 09:27:36 +0100

    Resolution of the screen.

--- Snake Game/screen.py
+++ Snake Game/screen.py
@@ -6,5 +6,7 @@ import pygame
 # New class : Windows :
 class Windows :
     # New function : __init__(self) :
-    def __init__(self) -> None:
-        pass
+    def __init__(self):
+        # Resolution of the screen :
+        self.screen = pygame.display.set_mode((800, 600))     # Double parentheses as a tuple.
+        
