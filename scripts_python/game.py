d6a8774a0e879bba39dbc329b2dd29165162dc41
theyadev <gtheya@gmail.com>
2021-12-06 13:33:41 +0100

    added socketio and players

--- scripts_python/game.py
+++ scripts_python/game.py
@@ -1,6 +1,7 @@
 # coding : utf-8
 
 import pygame
+import socketio
 from map import MapManager
 from player import Player
 
@@ -40,24 +41,75 @@ class Game:
 
     def run(self):
 
+        sio = socketio.Client()
+        @sio.event
+        def connect():
+            print('connection established')
+            sio.emit("JOIN", {
+            'pos': self.player.position,
+            'map': "world"
+            })
+
+        @sio.event
+        def disconnect():
+            print('disconnected from server')
+
+        try:
+            # sio.connect('http://93.6.41.243:8271/')
+            sio.connect('http://localhost:6969/')
+        except: 
+            pass
+
         # gerer les FPS
         clock = pygame.time.Clock()
         
         # boucle du jeu
         running = True
 
+        players = []
+
+        @sio.on("GET")
+        def addPlayers(data):
+            players.clear()
+            players.extend(data)
+
         while running:
+            sio.emit("GET")
 
             self.player.save_location()
             self.handle_input()
             self.update()
+            
+
+            if (self.player.old_position != self.player.position):
+                print(self.map_manager.current_map)
+                sio.emit("UPDATE POS", {
+                    'pos': self.player.position,
+                    'map': self.map_manager.current_map
+                })
+
+
+            for player in players:
+                if player['id'] == sio.get_sid():
+                    continue
+                
+                # print(player['map'])
+                if player['map'] != self.map_manager.current_map:
+                    continue
+
+                mul_player = Player(player['pos'][0],player['pos'][1])
+                mul_player.update()
+
+                self.map_manager.get_group().add(mul_player, layer=99)
+
             self.map_manager.draw()
             pygame.display.flip()
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
-
+            self.map_manager.get_group().remove_sprites_of_layer(99)
             clock.tick(60)
 
+        sio.disconnect()
         pygame.quit()
