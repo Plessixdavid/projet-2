7e467c772bad8aa2f1b451d00c60169393925dee
jorjormanie <pottier.jordan@gmail.com>
2021-12-20 15:38:43 +0100

    a

--- scripts_python/main.py
+++ scripts_python/main.py
@@ -1,7 +1,7 @@
 # coding  : utf-8
 
 import pygame
-import game
+from game import Game
 
 def Menuconsole():
     print("(1)Nouveaux personnage\t (2)Charger un personnage\t (3)Revenir au bureaux")
@@ -13,8 +13,8 @@ def Menuconsole():
         Email = input("quelle est votre email")
         Motdepasse = input("Choisissez un mot de passe")
         print("Je vous remercie , je vous souhaite un bon moment dans ce monde merveilleux. ")
-        # pygame.init()
-        # Game().run()
+        pygame.init()
+        Game().run()
     elif Choixmenu == 2 :
         pass
     elif Choixmenu == 3 :
